3c5ebde35fe5b9bd54092dd13a5b1030d79aa03a
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-23 12:14:48 -0700

    adding more pseudo code from  office hours

--- index.js
+++ index.js
@@ -6,3 +6,24 @@
 
 //View all employees
 //SELECT * FROM employee 
+
+// Create new departments 
+//Prompt the user for the "name" of the department
+//THEN Run the query 
+//INSERT INTO department (name)
+//VALUES ("Sales"); 
+
+    //THEN ask the user what they want to do next
+
+//Create a new role 
+
+// Get the existing departments from the 'department' table
+
+    //THEN prompt the user for the "title", "salary", and "department" for the role 
+
+        // THEN Run the query
+        //INSERT INTO role (title, salary, department_id)
+        //VALUES ("Manager:ex" 120000:ex, 1:ex)
+        // replace all examples above w/ question marks for query method
+
+            // THEN ask the user what they want to do next
