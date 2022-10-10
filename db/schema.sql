73f6249b9b6555a903060a9289f61751cea81fed
Zachary Warnes <zacharywarnes@gmail.com>
2022-10-10 15:46:38 -0700

    changing mysql syntax for view employees function to stop duplicate rows from appearing

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,27 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
     ON DELETE SET NULL,
--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const viewDepartments = () => {
 
 // View all roles 
 const viewRoles = () => {
-    const sql = `SELECT roles.id, roles.title, roles.department_id, roles.salary FROM roles`;
+    const sql = `SELECT * FROM roles`;
     db.query(sql, (err, rows) => {
         if (err) throw err;
         console.table(rows);
@@ -113,7 +113,7 @@ const viewRoles = () => {
 
 //View all employees
 const viewEmployees = () => {
-    const sql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary FROM employee LEFT JOIN roles ON employee.role_id LEFT JOIN department ON roles.department_id = department_id`;
+    const sql = `SELECT * FROM employee`;
 
     db.query(sql, (err, rows) =>{
         if (err) throw err;
@@ -242,7 +242,7 @@ const createEmployee = () => {
                             {
                                 type: 'list',
                                 message: "Who is the employee's manager?",
-                                choices: ['none', managers],
+                                choices: managers,
                                 name: 'manager'
                             }
                         ])
